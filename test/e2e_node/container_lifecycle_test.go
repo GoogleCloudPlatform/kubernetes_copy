kubernetes/kubernetes

e2e node: Delay probe for Evented PLEG known issue
This case expects the restartable init container to get into
CrashLoopBackoff before the regular container is stopped by failure of
its liveness probe. If Evented PLEG is enabled, it takes more seconds
till the init container gets into CrashLoopBackoff because the pod
worker is blocked at each probe failure. This fix delay the liveness
probe of the regular container as workaround for this known issue of
Evented PLEG.

test/e2e_node/container_lifecycle_test.go
@@ -923,6 +923,14 @@ var _ = SIGDescribe(framework.WithNodeConformance(), "Containers Lifecycle", fun
 							},
 						}
 
+						// FIXME: This case expects the restartable init container to get into CrashLoopBackoff before
+						//        the regular container is stopped by failure of its liveness probe.
+						//        If Evented PLEG is enabled, it takes more seconds till the init container gets into
+						//        CrashLoopBackoff because the pod worker is blocked at each probe failure (#127312).
+						if e2eskipper.IsFeatureGateEnabled(features.EventedPLEG) {
+							podSpec.Spec.Containers[0].LivenessProbe.InitialDelaySeconds = 40
+						}
+
 						preparePod(podSpec)
 
 						podSpec = client.Create(ctx, podSpec)

GoogleCloudPlatform/kubernetes_copy

test/e2e_node/container_lifecycle_test.go
@@ -923,6 +923,14 @@ var _ = SIGDescribe(framework.WithNodeConformance(), "Containers Lifecycle", fun
 							},
 						}
 
+						// FIXME: This case expects the restartable init container to get into CrashLoopBackoff before
+						//        the regular container is stopped by failure of its liveness probe.
+						//        If Evented PLEG is enabled, it takes more seconds till the init container gets into
+						//        CrashLoopBackoff because the pod worker is blocked at each probe failure (#127312).
+						if e2eskipper.IsFeatureGateEnabled(kubefeatures.EventedPLEG) {
+							podSpec.Spec.Containers[0].LivenessProbe.InitialDelaySeconds = 40
+						}
+
 						preparePod(podSpec)
 
 						podSpec = client.Create(ctx, podSpec)