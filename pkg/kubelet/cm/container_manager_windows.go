ajcaldelas/kubernetes

node: cpumgr: migrate to contextual logging
migrate the cpumanager code to contextual logging

Signed-off-by: Francesco Romani <fromani@redhat.com>

pkg/kubelet/cm/container_manager_windows.go
@@ -118,7 +118,7 @@ func (cm *containerManagerImpl) Start(ctx context.Context, node *v1.Node,
 }
 
 // NewContainerManager creates windows container manager.
-func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.Interface, nodeConfig NodeConfig, failSwapOn bool, recorder record.EventRecorder, kubeClient clientset.Interface) (ContainerManager, error) {
+func NewContainerManager(ctx context.Context, mountUtil mount.Interface, cadvisorInterface cadvisor.Interface, nodeConfig NodeConfig, failSwapOn bool, recorder record.EventRecorder, kubeClient clientset.Interface) (ContainerManager, error) {
 	// It is safe to invoke `MachineInfo` on cAdvisor before logically initializing cAdvisor here because
 	// machine info is computed and cached once as part of cAdvisor object creation.
 	// But `RootFsInfo` and `ImagesFsInfo` are not available at this moment so they will be called later during manager starts
@@ -134,8 +134,10 @@ func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.I
 		cadvisorInterface: cadvisorInterface,
 	}
 
+	logger := klog.FromContext(ctx)
+
 	cm.topologyManager = topologymanager.NewFakeManager()
-	cm.cpuManager = cpumanager.NewFakeManager()
+	cm.cpuManager = cpumanager.NewFakeManager(logger)
 	cm.memoryManager = memorymanager.NewFakeManager()
 
 	if utilfeature.DefaultFeatureGate.Enabled(kubefeatures.WindowsCPUAndMemoryAffinity) {
@@ -151,6 +153,7 @@ func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.I
 
 		klog.InfoS("Creating cpu manager")
 		cm.cpuManager, err = cpumanager.NewManager(
+			ctx,
 			nodeConfig.CPUManagerPolicy,
 			nodeConfig.CPUManagerPolicyOptions,
 			nodeConfig.CPUManagerReconcilePeriod,

pkg/kubelet/cm/cpumanager/fake_cpu_manager.go
@@ -17,7 +17,10 @@ limitations under the License.
 package cpumanager
 
 import (
-	"k8s.io/api/core/v1"
+	"context"
+
+	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/kubelet/cm/containermap"
 	"k8s.io/kubernetes/pkg/kubelet/cm/cpumanager/state"
@@ -28,41 +31,43 @@ import (
 )
 
 type fakeManager struct {
-	state state.State
+	logger logr.Logger
+	state  state.State
 }
 
 func (m *fakeManager) Start(activePods ActivePodsFunc, sourcesReady config.SourcesReady, podStatusProvider status.PodStatusProvider, containerRuntime runtimeService, initialContainers containermap.ContainerMap) error {
-	klog.InfoS("Start()")
+	m.logger.Info("Start()")
 	return nil
 }
 
 func (m *fakeManager) Policy() Policy {
-	klog.InfoS("Policy()")
-	pol, _ := NewNonePolicy(nil)
+	m.logger.Info("Policy()")
+	ctx := klog.NewContext(context.Background(), m.logger)
+	pol, _ := NewNonePolicy(ctx, nil)
 	return pol
 }
 
 func (m *fakeManager) Allocate(pod *v1.Pod, container *v1.Container) error {
-	klog.InfoS("Allocate", "pod", klog.KObj(pod), "containerName", container.Name)
+	m.logger.Info("Allocate", "pod", klog.KObj(pod), "containerName", container.Name)
 	return nil
 }
 
 func (m *fakeManager) AddContainer(pod *v1.Pod, container *v1.Container, containerID string) {
-	klog.InfoS("AddContainer", "pod", klog.KObj(pod), "containerName", container.Name, "containerID", containerID)
+	m.logger.Info("AddContainer", "pod", klog.KObj(pod), "containerName", container.Name, "containerID", containerID)
 }
 
 func (m *fakeManager) RemoveContainer(containerID string) error {
-	klog.InfoS("RemoveContainer", "containerID", containerID)
+	m.logger.Info("RemoveContainer", "containerID", containerID)
 	return nil
 }
 
 func (m *fakeManager) GetTopologyHints(pod *v1.Pod, container *v1.Container) map[string][]topologymanager.TopologyHint {
-	klog.InfoS("Get container topology hints")
+	m.logger.Info("Get container topology hints")
 	return map[string][]topologymanager.TopologyHint{}
 }
 
 func (m *fakeManager) GetPodTopologyHints(pod *v1.Pod) map[string][]topologymanager.TopologyHint {
-	klog.InfoS("Get pod topology hints")
+	m.logger.Info("Get pod topology hints")
 	return map[string][]topologymanager.TopologyHint{}
 }
 
@@ -71,17 +76,17 @@ func (m *fakeManager) State() state.Reader {
 }
 
 func (m *fakeManager) GetExclusiveCPUs(podUID, containerName string) cpuset.CPUSet {
-	klog.InfoS("GetExclusiveCPUs", "podUID", podUID, "containerName", containerName)
+	m.logger.Info("GetExclusiveCPUs", "podUID", podUID, "containerName", containerName)
 	return cpuset.CPUSet{}
 }
 
 func (m *fakeManager) GetAllocatableCPUs() cpuset.CPUSet {
-	klog.InfoS("Get Allocatable CPUs")
+	m.logger.Info("Get Allocatable CPUs")
 	return cpuset.CPUSet{}
 }
 
 func (m *fakeManager) GetCPUAffinity(podUID, containerName string) cpuset.CPUSet {
-	klog.InfoS("GetCPUAffinity", "podUID", podUID, "containerName", containerName)
+	m.logger.Info("GetCPUAffinity", "podUID", podUID, "containerName", containerName)
 	return cpuset.CPUSet{}
 }
 
@@ -91,8 +96,9 @@ func (m *fakeManager) GetAllCPUs() cpuset.CPUSet {
 }
 
 // NewFakeManager creates empty/fake cpu manager
-func NewFakeManager() Manager {
+func NewFakeManager(logger logr.Logger) Manager {
 	return &fakeManager{
-		state: state.NewMemoryState(),
+		logger: logger,
+		state:  state.NewMemoryState(logger),
 	}
 }

GoogleCloudPlatform/kubernetes_copy

pkg/kubelet/cm/container_manager_windows.go
@@ -118,7 +118,7 @@ func (cm *containerManagerImpl) Start(ctx context.Context, node *v1.Node,
 }
 
 // NewContainerManager creates windows container manager.
-func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.Interface, nodeConfig NodeConfig, failSwapOn bool, recorder record.EventRecorder, kubeClient clientset.Interface) (ContainerManager, error) {
+func NewContainerManager(ctx context.Context, mountUtil mount.Interface, cadvisorInterface cadvisor.Interface, nodeConfig NodeConfig, failSwapOn bool, recorder record.EventRecorder, kubeClient clientset.Interface) (ContainerManager, error) {
 	// It is safe to invoke `MachineInfo` on cAdvisor before logically initializing cAdvisor here because
 	// machine info is computed and cached once as part of cAdvisor object creation.
 	// But `RootFsInfo` and `ImagesFsInfo` are not available at this moment so they will be called later during manager starts
@@ -135,7 +135,7 @@ func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.I
 	}
 
 	cm.topologyManager = topologymanager.NewFakeManager()
-	cm.cpuManager = cpumanager.NewFakeManager()
+	cm.cpuManager = cpumanager.NewFakeManager(ctx)
 	cm.memoryManager = memorymanager.NewFakeManager()
 
 	if utilfeature.DefaultFeatureGate.Enabled(kubefeatures.WindowsCPUAndMemoryAffinity) {
@@ -151,6 +151,7 @@ func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.I
 
 		klog.InfoS("Creating cpu manager")
 		cm.cpuManager, err = cpumanager.NewManager(
+			ctx,
 			nodeConfig.CPUManagerPolicy,
 			nodeConfig.CPUManagerPolicyOptions,
 			nodeConfig.CPUManagerReconcilePeriod,

pkg/kubelet/cm/cpumanager/fake_cpu_manager.go
@@ -17,7 +17,10 @@ limitations under the License.
 package cpumanager
 
 import (
-	"k8s.io/api/core/v1"
+	"context"
+
+	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/kubelet/cm/containermap"
 	"k8s.io/kubernetes/pkg/kubelet/cm/cpumanager/state"
@@ -28,41 +31,43 @@ import (
 )
 
 type fakeManager struct {
-	state state.State
+	logger logr.Logger
+	state  state.State
 }
 
 func (m *fakeManager) Start(activePods ActivePodsFunc, sourcesReady config.SourcesReady, podStatusProvider status.PodStatusProvider, containerRuntime runtimeService, initialContainers containermap.ContainerMap) error {
-	klog.InfoS("Start()")
+	m.logger.Info("Start()")
 	return nil
 }
 
 func (m *fakeManager) Policy() Policy {
-	klog.InfoS("Policy()")
-	pol, _ := NewNonePolicy(nil)
+	m.logger.Info("Policy()")
+	ctx := klog.NewContext(context.Background(), m.logger)
+	pol, _ := NewNonePolicy(ctx, nil)
 	return pol
 }
 
 func (m *fakeManager) Allocate(pod *v1.Pod, container *v1.Container) error {
-	klog.InfoS("Allocate", "pod", klog.KObj(pod), "containerName", container.Name)
+	m.logger.Info("Allocate", "pod", klog.KObj(pod), "containerName", container.Name)
 	return nil
 }
 
 func (m *fakeManager) AddContainer(pod *v1.Pod, container *v1.Container, containerID string) {
-	klog.InfoS("AddContainer", "pod", klog.KObj(pod), "containerName", container.Name, "containerID", containerID)
+	m.logger.Info("AddContainer", "pod", klog.KObj(pod), "containerName", container.Name, "containerID", containerID)
 }
 
 func (m *fakeManager) RemoveContainer(containerID string) error {
-	klog.InfoS("RemoveContainer", "containerID", containerID)
+	m.logger.Info("RemoveContainer", "containerID", containerID)
 	return nil
 }
 
 func (m *fakeManager) GetTopologyHints(pod *v1.Pod, container *v1.Container) map[string][]topologymanager.TopologyHint {
-	klog.InfoS("Get container topology hints")
+	m.logger.Info("Get container topology hints")
 	return map[string][]topologymanager.TopologyHint{}
 }
 
 func (m *fakeManager) GetPodTopologyHints(pod *v1.Pod) map[string][]topologymanager.TopologyHint {
-	klog.InfoS("Get pod topology hints")
+	m.logger.Info("Get pod topology hints")
 	return map[string][]topologymanager.TopologyHint{}
 }
 
@@ -71,28 +76,30 @@ func (m *fakeManager) State() state.Reader {
 }
 
 func (m *fakeManager) GetExclusiveCPUs(podUID, containerName string) cpuset.CPUSet {
-	klog.InfoS("GetExclusiveCPUs", "podUID", podUID, "containerName", containerName)
+	m.logger.Info("GetExclusiveCPUs", "podUID", podUID, "containerName", containerName)
 	return cpuset.CPUSet{}
 }
 
 func (m *fakeManager) GetAllocatableCPUs() cpuset.CPUSet {
-	klog.InfoS("Get Allocatable CPUs")
+	m.logger.Info("Get Allocatable CPUs")
 	return cpuset.CPUSet{}
 }
 
 func (m *fakeManager) GetCPUAffinity(podUID, containerName string) cpuset.CPUSet {
-	klog.InfoS("GetCPUAffinity", "podUID", podUID, "containerName", containerName)
+	m.logger.Info("GetCPUAffinity", "podUID", podUID, "containerName", containerName)
 	return cpuset.CPUSet{}
 }
 
 func (m *fakeManager) GetAllCPUs() cpuset.CPUSet {
-	klog.InfoS("GetAllCPUs")
+	m.logger.Info("GetAllCPUs")
 	return cpuset.CPUSet{}
 }
 
 // NewFakeManager creates empty/fake cpu manager
-func NewFakeManager() Manager {
+func NewFakeManager(ctx context.Context) Manager {
+	logger := klog.LoggerWithName(klog.FromContext(ctx), "cpu.fake")
 	return &fakeManager{
-		state: state.NewMemoryState(),
+		logger: logger,
+		state:  state.NewMemoryState(logger),
 	}
 }